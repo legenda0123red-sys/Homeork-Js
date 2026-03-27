04e70ea85ebd509a273b1be4b44fd1ad584ee493
legenda0123red-sys <legenda0123red@gmail.com>
2026-03-27 22:43:09 +0600

    function2

--- Homework7/HW7.js
+++ Homework7/HW7.js
@@ -1,4 +1,17 @@
-const sayHello = (name, surname) => {
-    return `Здраствуйте, ${surname} ${name}. Рады вас видеть на курсах Metalabs`
+// const sayHello = (name, surname) => {
+//     return `Здраствуйте, ${surname} ${name}. Рады вас видеть на курсах Metalabs`
+// }
+// console.log(sayHello('Sanjar', 'cr7'));
+
+const MasterYoda = (num) => {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'MasterYoda';
+    } else if (num % 3 === 0) {
+        return 'Master'
+    } else if (num % 5 === 0) {
+        return 'Yoda'
+    }
 }
-console.log();
+console.log(MasterYoda(9));
+console.log(MasterYoda(10));
+console.log(MasterYoda(15));
