5a2abb0e75e197da0463f582cf6526700c80b7d3
legenda0123red-sys <legenda0123red@gmail.com>
2026-03-28 22:13:18 +0600

    методы массива 1

--- Homework7/HW7.js
+++ Homework7/HW7.js
@@ -3,15 +3,33 @@
 // }
 // console.log(sayHello('Sanjar', 'cr7'));
 
-const MasterYoda = (num) => {
-    if (num % 3 === 0 && num % 5 === 0) {
-        return 'MasterYoda';
-    } else if (num % 3 === 0) {
-        return 'Master'
-    } else if (num % 5 === 0) {
-        return 'Yoda'
-    }
-}
-console.log(MasterYoda(9));
-console.log(MasterYoda(10));
-console.log(MasterYoda(15));
+// const MasterYoda = (num) => {
+//     if (num % 3 === 0 && num % 5 === 0) {
+//         return 'MasterYoda';
+//     } else if (num % 3 === 0) {
+//         return 'Master'
+//     } else if (num % 5 === 0) {
+//         return 'Yoda'
+//     }
+// }
+// console.log(MasterYoda(9));
+// console.log(MasterYoda(10));
+// console.log(MasterYoda(15));
+
+
+// 1.  У вас есть массив чисел. Создайте новый массив, где каждое число возведено в квадрат.
+// [2, 4, 6, 8];
+// // Ожидаемый результат: [4, 16, 36, 64]
+// 2. Из массива возрастов оставьте только те, чей возраст больше 27
+// [12, 18, 27, 39, 40]
+// // Ожидаемый результат [27, 39, 40]
+// Task1
+// const arr = [ 2, 3, 4]
+// const getSquare = arr.map((square) => square ** 2);
+// console.log(getSquare);
+ 
+// Task2
+// const task2 = (arr) => {
+//     return arr.filter((item) => item >= 27)
+// }
+// console.log(task2([12, 18, 27, 39, 40]));
