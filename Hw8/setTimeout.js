c7c23baf5802961ac4cbd092a9bd014a9a2444b8
legenda0123red-sys <legenda0123red@gmail.com>
2026-05-07 13:13:08 +0600

    Fetch

--- Hw8/setTimeout.js
+++ Hw8/setTimeout.js
@@ -26,28 +26,69 @@
 // }
 
 
-const peopleNum = +prompt('Сколько придут людей');
-const placeCon = +prompt('Сколько свободных мест');
+// const peopleNum = +prompt('Сколько придут людей');
+// const placeCon = +prompt('Сколько свободных мест');
 
-const promise = new Promise((res, rej) => {
-    if (peopleNum <= placeCon) {
-        const freePlaces = placeCon - peopleNum;
-        res(freePlaces);
-    } else {
-        rej('Мест не хватает!');
-    }
-});
+// const promise = new Promise((res, rej) => {
+//     if (peopleNum <= placeCon) {
+//         const freePlaces = placeCon - peopleNum;
+//         res(freePlaces);
+//     } else {
+//         rej('Мест не хватает!');
+//     }
+// });
 
-promise
-  .then((freePlaces) => {
-    console.log(`Свободных мест: ${freePlaces}`);
+// promise
+//   .then((freePlaces) => {
+//     console.log(`Свободных мест: ${freePlaces}`);
+//   })
+//   .then(() => {
+//     console.log('Добро пожаловать!!!');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   })
+//   .finally(() => {
+//     console.log('Если что вернем деньги');
+//   });
+
+// GET
+fetch('https://fakestoreapi.com/carts')
+  .then(res => res.json())
+  .then(json => console.log(json))
+
+
+// POST
+fetch('https://fakestoreapi.com/carts', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    userId: 1
   })
-  .then(() => {
-    console.log('Добро пожаловать!!!');
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
+
+
+// PUT
+fetch('https://fakestoreapi.com/carts/1', {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    userId: 2
   })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    console.log('Если что вернем деньги');
-  });
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
+
+
+// DELETE
+fetch('https://fakestoreapi.com/carts/1', {
+  method: 'DELETE'
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
